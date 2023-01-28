fdab61d4aa296a295b8a68fe8f3ad62dddecb7bf
Owen Duncan-Snobel <owenduncansnobel@gmail.com>
2023-01-28 12:37:34 -0500

    move files to another repo
    Co-Authored-By: KoalaBiceps <113807667+KoalaBiceps@users.noreply.github.com>
    Co-Authored-By: Daniel Liu <68881910+Teaapoo@users.noreply.github.com>
    Co-Authored-By: Thanh Phat (Theo) Lam <62038195+theo-thanhlam@users.noreply.github.com>

---
+++ client/pages/login.tsx
@@ -0,0 +1,13 @@
+
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Login = () => {
+    return (
+        <div>
+            Hello from login
+        </div>
+    )
+}
+export default Login
