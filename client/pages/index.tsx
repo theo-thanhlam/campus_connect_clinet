eebc33b54dad4138d3c5fd2756c12f3459c3a430
Owen Duncan-Snobel <owenduncansnobel@gmail.com>
2023-01-28 12:22:32 -0500

    Update index.tsx
    Co-Authored-By: KoalaBiceps <113807667+KoalaBiceps@users.noreply.github.com>
    Co-Authored-By: Teaapoo <68881910+Teaapoo@users.noreply.github.com>
    Co-Authored-By: Thanh Phat (Theo) Lam <62038195+theo-thanhlam@users.noreply.github.com>

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -31,9 +31,10 @@ const Home: NextPage = () => {
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
+            <h3 className="text-2xl font-bold">Information &rarr;</h3>
             <p className="mt-4 text-xl">
               Find in-depth information about Next.js features and its API.
+              try to get commit
             </p>
           </a>
 
