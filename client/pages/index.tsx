039f26871dc5447555ff0a49c93da664ef50e50a
Owen Duncan-Snobel <owenduncansnobel@gmail.com>
2023-01-28 15:36:48 -0500

    login and display
    Co-Authored-By: KoalaBiceps <113807667+KoalaBiceps@users.noreply.github.com>
    Co-Authored-By: Daniel Liu <68881910+Teaapoo@users.noreply.github.com>
    Co-Authored-By: Thanh Phat (Theo) Lam <62038195+theo-thanhlam@users.noreply.github.com>

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,9 +2,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
+
+
 
 const Home: NextPage = () => {
-  
+  const session = useSession()
+  const supabase = useSupabaseClient()
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
@@ -15,73 +20,37 @@ const Home: NextPage = () => {
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
+          <a className="text-blue-600">
             YorkU Note Sharing App
           </a>
         </h1>
 
-        {/* <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p> */}
-
         <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
+
+
           <a
-            href="https://nextjs.org/docs"
+            href="/login"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Information &rarr;</h3>
+            <h3 className="text-2xl font-bold">Login &rarr;</h3>
             <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-              try to get commit
+              Go login now
             </p>
           </a>
 
           <a
-            href="https://nextjs.org/learn"
+            href="/display"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
+            <h3 className="text-2xl font-bold">Note &rarr;</h3>
             <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Find note for your class
             </p>
           </a>
         </div>
       </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+
     </div>
   )
 }
--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -1,13 +1,66 @@
-
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useSession, useSupabaseClient, useUser } from '@supabase/auth-helpers-react'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 const Login = () => {
+  const supabase = useSupabaseClient()
+  const session = useSession()
+  const user = useUser()
+  const router = useRouter()
+  
+  useEffect(() => {
+    async function redirectUser() {
+        const { from } = router.query
+        if (from) {
+          router.push(from as string)
+        } else {
+          router.push('/')
+        }
+      }
+      if (user) redirectUser()
+  })
+  
+  async function signInWithGitHub() {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'github',
+    })
+  } 
+
     return (
-        <div>
-            Hello from login
-        </div>
+        <section className="h-screen">
+          <div className="px-6 h-full text-gray-800">
+              <div
+                className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6"
+              >
+                <div
+                  className="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0"
+                >
+                    <img
+                        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                      className="w-full"
+                        alt="Sample image"
+                    />
+                </div>
+                <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
+                  <form>
+                    <div className="text-center lg:text-left">
+                        <button
+                            onClick={signInWithGitHub}
+                            type="button"
+                            className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                        >
+                            Login with GitHub
+                        </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+        </section>
     )
 }
+
 export default Login
